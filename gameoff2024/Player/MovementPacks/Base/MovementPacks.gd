77b5b81bfe9c3bb324b9c61486f94537d8bebad1
Luth <noafowl@gmail.com>
2024-11-05 22:23:27 -0700

    Movement shit

--- gameoff2024/Player/MovementPacks/Base/MovementPacks.gd
+++ gameoff2024/Player/MovementPacks/Base/MovementPacks.gd
@@ -1,1 +1,7 @@
 extends Resource
+class_name MovementPack
+
+@export var speed: float
+@export var acc: float
+@export var frict: float
+@export var run_speed: float
--- gameoff2024/Player/player.gd
+++ gameoff2024/Player/player.gd
@@ -1,1 +1,16 @@
 extends CharacterBody2D
+
+@export var mp: MovementPack
+
+func move():
+	pass
+
+func get_input():
+	var x_axis = 0
+	
+	if Input.is_action_pressed("left"):
+		x_axis = -1 
+	elif Input.is_action_pressed("right"):
+		x_axis = 1 
+	
+	return x_axis
