b8c6c300727369b710cfd195738937dd88e92496
Luth <noafowl@gmail.com>
2024-11-12 11:21:51 -0700

    Fixed the anims a bit

--- gameoff2024/Player/player.gd
+++ gameoff2024/Player/player.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
-var current_anim: String
+var idle_anim = "foreward"
 
 func _physics_process(delta: float):
 	move(delta)
@@ -14,13 +14,11 @@ func move(delta):
 	
 	if input == Vector2.ZERO:
 		frict(delta)
-		current_anim = "idle"
-		animated_sprite_2d.flip_h = false
 		
 	elif input != Vector2.ZERO:
 		acc(delta, input)
 	
-	animated_sprite_2d.play(current_anim)
+	play_anims()
 	move_and_slide()
 
 func get_input():
@@ -28,23 +26,15 @@ func get_input():
 	
 	if Input.is_action_pressed("left"):
 		input_axis.x = -1
-		current_anim = "walk_side"
-		animated_sprite_2d.flip_h = true
 		
 	elif Input.is_action_pressed("right"):
 		input_axis.x = 1
-		current_anim = "walk_side"
-		animated_sprite_2d.flip_h = false
 
 	if Input.is_action_pressed("up"):
 		input_axis.y = -1
-		current_anim = "walk_back"
-		animated_sprite_2d.flip_h = false
 		
 	elif Input.is_action_pressed("down"):
 		input_axis.y = 1 
-		current_anim = "walk_foreward"
-		animated_sprite_2d.flip_h = false
 	
 	return input_axis
 
@@ -61,4 +51,29 @@ func frict(delta):
 	velocity.y = move_toward(velocity.y, 0, mp.frict * delta)
 
 func play_anims():
-	pass # Get this later plss 
+	var current_anim: String
+
+	if velocity.y > 0:
+		current_anim = "walk_foreward"
+		idle_anim = "foreward"
+		animated_sprite_2d.flip_h = false
+		
+	elif velocity.y < 0:
+		current_anim = "walk_back"
+		idle_anim = "back"
+		animated_sprite_2d.flip_h = false
+		
+	elif velocity.x > 0:
+		current_anim = "walk_side"
+		idle_anim = "side"
+		animated_sprite_2d.flip_h = false
+		
+	elif velocity.x < 0:
+		current_anim = "walk_side"
+		idle_anim = "side"
+		animated_sprite_2d.flip_h = true
+	
+	if velocity == Vector2.ZERO:
+		current_anim = "idle_" + idle_anim
+	
+	animated_sprite_2d.play(current_anim)
