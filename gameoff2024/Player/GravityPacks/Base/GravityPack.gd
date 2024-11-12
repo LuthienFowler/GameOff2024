becc4c1cada46b310adbf62bb1c294ea77f1ea68
Luth <noafowl@gmail.com>
2024-11-12 10:46:37 -0700

    Changed the movement a bit and organization

--- gameoff2024/Player/GravityPacks/Base/GravityPack.gd
+++
@@ -1,6 +0,0 @@
-extends Resource
-class_name GravityPack
-
-@export var jump_height: float
-@export var jump_time_to_peak: float
-@export var jump_time_to_descent: float
--- gameoff2024/Player/player.gd
+++ gameoff2024/Player/player.gd
@@ -1,49 +1,43 @@
 extends CharacterBody2D
 
 @export var mp: MovementPack
-@export var gp: GravityPack
-
-@onready var jump_velocity = ((2.0 * gp.jump_height) / gp.jump_time_to_peak) * -1 
-@onready var jump_gravity = ((-2.0 * gp.jump_height) / (gp.jump_time_to_peak * gp.jump_time_to_peak)) * -1
-@onready var fall_gravity = ((-2.0 * gp.jump_height) / (gp.jump_time_to_descent * gp.jump_time_to_descent)) * -1
 
 func _physics_process(delta: float):
 	move(delta)
-	jump()
-	velocity.y += apply_gravity() * delta
 
 func move(delta):
-	var input = get_input()
+	var input = get_input().normalized()
 	
-	if input == 0:
+	if input == Vector2.ZERO:
 		frict(delta)
-	elif input != 0:
+	elif input != Vector2.ZERO:
 		acc(delta, input)
 	
 	move_and_slide()
 
 func get_input():
-	var x_axis = 0
+	var input_axis = Vector2.ZERO
 	
 	if Input.is_action_pressed("left"):
-		x_axis = -1 
+		input_axis.x = -1
 	elif Input.is_action_pressed("right"):
-		x_axis = 1 
+		input_axis.x = 1
+
+	if Input.is_action_pressed("up"):
+		input_axis.y = -1
+	elif Input.is_action_pressed("down"):
+		input_axis.y = 1 
 	
-	return x_axis
+	return input_axis
 
 func acc(delta, direction):
 	if Input.is_action_pressed("run"):
-		velocity.x = move_toward(velocity.x, mp.run_speed * direction, mp.acc * delta)
+		velocity.x = move_toward(velocity.x, mp.run_speed * direction.x, mp.acc * delta)
+		velocity.y = move_toward(velocity.y, mp.run_speed * direction.y, mp.acc *delta)
 	else:
-		velocity.x = move_toward(velocity.x, mp.speed * direction, mp.acc * delta)
+		velocity.x = move_toward(velocity.x, mp.speed * direction.x, mp.acc * delta)
+		velocity.y = move_toward(velocity.y, mp.speed * direction.y, mp.acc * delta)
 
 func frict(delta):
 	velocity.x = move_toward(velocity.x, 0, mp.frict * delta)
-
-func apply_gravity():
-	return jump_gravity if velocity.y < 0.0 else fall_gravity
-
-func jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = jump_velocity
+	velocity.y = move_toward(velocity.y, 0, mp.frict * delta)
